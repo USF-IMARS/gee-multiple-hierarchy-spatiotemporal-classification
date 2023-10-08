193730729d5f0a12e3bcf3f87a340d33a6a688d9
Tylar <email@tylar.info>
2023-10-08 16:24:00 -0400

    Create publicDwCFeatureCollections.js

---
+++ publicDwCFeatureCollections.js
@@ -0,0 +1,1 @@
+{"assetId":"/users/Tylar_Murray/DwCFC-deep_water-st_thomas-COPERNICUS_-S2_SR_-20210124T145731_20210124T145726_T20QKF","class_name":"deep_water","roi_name":"st_thomas","referenceImageID":"COPERNICUS/S2_SR/20210124T145731_20210124T145726_T20QKF","identifiedBy":"Tylar_Murray"}
